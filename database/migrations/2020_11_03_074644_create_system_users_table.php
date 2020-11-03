975adb69080d8ded6ae982fc7f1e44b8dbe86c96
Alvin_Gayao <yanah092619>
2020-11-03 15:59:55 +0800

    my database migration commits

---
+++ database/migrations/2020_11_03_074644_create_system_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSystemUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('system_users', function (Blueprint $table) {
+            $table->id();
+            $tale->char('username', 255);
+            $table->char('password', 255);
+            $table->string('usertype');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('system_users');
+    }
+}
