ce4f8374322c084d26124c2c47d5b18c5cdbfbe7
Marco Raphael San Jose <marcoraphaelsanjose1@yahoo.com>
2020-11-13 10:39:32 +0800

    ProductsController edit

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,5 +6,48 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+public function index()
+    {
+    	$products = Products::all();
+    	return view('products.index', compact('products'));
+    }
+
+    public function create()
+    {
+    	return view('products.create');
+    }
+
+    public function store()
+    {
+    	$products = new Products;
+    	$products->itemName = request()->itemName;
+    	$products->itemDescription = request()->itemDescription;
+    	$products->price = request()->price;
+    	$products->save();
+
+    	//redirect to products page
+    	return redirect('/products');
+
+    }
+
+    public function edit(Products $products)
+    {
+    	return view('products.edit', compact('products'));
+    }
+
+    public function update(Products $products)
+    {
+    	$products->itemName = request()->itemName;
+    	$products->itemDescription = request()->itemDescription;
+    	$products->price = request()->price;
+    	$products->save();
+    	//redirect to products page
+    	return redirect('/products');
+    }
+
+    public function destroy(Products $products)
+    {
+    	$products->delete();
+    	return redirect('/products');
+    }
 }
